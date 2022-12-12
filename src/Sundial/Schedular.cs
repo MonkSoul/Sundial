8f8725e9fa4d98ae60cd55cebfdcd4851b497aae
MonkSoul <monksoul@outlook.com>
2022-12-12 10:33:15 +0800

    发布 v2.5.5 版本，新增 `Schedular` 静态类

---
+++ src/Sundial/Schedular.cs
@@ -0,0 +1,51 @@
+﻿// MIT License
+//
+// Copyright (c) 2020-2022 百小僧, Baiqian Co.,Ltd and Contributors
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+namespace Sundial;
+
+/// <summary>
+/// 作业调度器静态类
+/// </summary>
+public static class Schedular
+{
+    /// <summary>
+    /// 序列化对象
+    /// </summary>
+    /// <remarks>主要用于作业触发器参数，作业信息额外数据序列化</remarks>
+    /// <param name="obj">对象</param>
+    /// <returns><see cref="string"/></returns>
+    public static string Serialize(object obj)
+    {
+        return Penetrates.Serialize(obj);
+    }
+
+    /// <summary>
+    /// 反序列化对象
+    /// </summary>
+    /// <remarks>主要用于作业触发器参数，作业信息额外数据序列化</remarks>
+    /// <param name="json">JSON 字符串</param>
+    /// <returns>T</returns>
+    public static T Deserialize<T>(string json)
+    {
+        return Penetrates.Deserialize<T>(json);
+    }
+}
