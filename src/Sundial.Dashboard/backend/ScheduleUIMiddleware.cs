ee49df29a798c8ab4046d2262fca367dbe70e592
MonkSoul <monksoul@outlook.com>
2023-04-06 11:55:08 +0800

    发布 v2.9.8 版本，改进动态作业委托持久化方式

--- src/Sundial.Dashboard/backend/ScheduleUIMiddleware.cs
+++ src/Sundial.Dashboard/backend/ScheduleUIMiddleware.cs
@@ -93,7 +93,7 @@ public sealed class ScheduleUIMiddleware
             using (var readStream = currentAssembly.GetManifestResourceStream($"{currentAssembly.GetName().Name}.frontend.apiconfig.js"))
             {
                 buffer = new byte[readStream.Length];
-                readStream.Read(buffer, 0, buffer.Length);
+                await readStream.ReadAsync(buffer);
             }
 
             // 替换配置占位符
@@ -101,8 +101,8 @@ public sealed class ScheduleUIMiddleware
             using (var stream = new MemoryStream(buffer))
             {
                 using var streamReader = new StreamReader(stream, new UTF8Encoding(false));
-                content = streamReader.ReadToEnd()
-                                      .Replace("%(RequestPath)", RequestPath);
+                content = (await streamReader.ReadToEndAsync())
+                                             .Replace("%(RequestPath)", RequestPath);
             }
 
             // 输出到客户端
